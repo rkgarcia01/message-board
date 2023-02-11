d38cd086a2b949d88a6db6ad5f9ea31e0f53fd6a
Reyki <reykigarcia@gmail.com>
2023-02-11 17:10:11 -0600

    Added Tests

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# To help us build new database models which will "model" thet characteristics of the data in our database, we import a module called "models" from django.db library
 # We've created a new database model called post which has the database field text.
 # We've also specified the type of content it will hold, "TextField()"
 class Post(models.Model):
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+
+class PostTests(TestCase):
+    # This part checks to see if the post a user makes is equal to what is stored in the database
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    """
+    def test_urls_avaialbe_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "This is a test!")
+    """
+    # This function combines all the other three functions in the quottions together using one response
+    def test_homepage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a test!")
